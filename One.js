c7af88452fe1c04d84fb9db625fbf9bb4b7cf4d1
DanielQ-arch <drq@njit.edu>
2024-11-21 09:07:31 -0500

    update

--- One.js
+++ One.js
@@ -1,4 +1,36 @@
-let characters = ['Harry', 'Hermione', 'Ron', 'Draco', 'Luna']
+let characters = ['Harry', 'Hermione', 'Ron', 'Draco', 'Luna'];
 
-for (let i = 0; i < characters.length; i++)
-    console.log(characters[i])
+for (let i = 0; i < characters.length; i++);
+    console.log(characters[i]);
+
+
+var firstName = 'Harry';
+
+var house = 'Gryffindor';
+
+console.log("Welcome, ${firstName} of ${Gryffindor}");
+
+
+var characterName = 'Hermione';
+
+console.log(characterName.toLowerCase());
+console.log(characterName.toUpperCase());
+
+var spell = 'Expelliarmus';
+
+console.log(spell.trim());
+
+
+var quote = 'I solemnly swear that I am up to no good';
+
+console.log(quote.slice());
+
+
+var firstName = 'Ron';
+var lastName = 'Weasley';
+
+console.log(firstName.concat('',lastName));
+
+var sentence = 'Draco is a good wizard';
+
+console.log(sentence.replace('good', 'bad'));
